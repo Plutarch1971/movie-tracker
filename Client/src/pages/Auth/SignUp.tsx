cb4a33db7da122a6b610a21400dc95f0521c5cbb
Matt-Verni <matthewpmendez@gmail.com>
2024-12-23 18:55:35 -0500

    Deleted Root Level Node Modules

---
+++ Client/src/pages/Auth/SignUp.tsx
@@ -0,0 +1,13 @@
+function SignUp () {
+    return (
+        <>
+        <div><h1>Sign Up Page</h1></div>
+        <div>
+            <label htmlFor="username">Enter username:</label>
+            
+        </div>
+        </>
+    )
+
+}
+export default SignUp;
