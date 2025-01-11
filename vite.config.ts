dfac6c825917cd09b7035bd26fca38a8966f5276
CamBC-Lab <branden.camilo@yahoo.com>
2025-01-11 17:00:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,36 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import envCompatible from 'vite-plugin-env-compatible';
+import envCompatible from 'vite-plugin-env-compatible'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
+  root: './client/src', // Add this to specify source directory
+  build: {
+    outDir: path.resolve(dirname, 'dist'), // Specify build output directory
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(dirname, 'client/src/index.html') // Specify entry point
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
+    setupFiles: './src/tests/setup.ts'
   },
   server: {
     port: 5173,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3001/',
         secure: false,
         changeOrigin: true
       },
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3001/',
         secure: false,
         changeOrigin: true
       }
